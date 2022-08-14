4eec164621947222a40d03627e698df478ff187e
YutoKashiwagi <kashiwagi19950714@gmail.com>
2022-08-14 23:13:07 +0900

    テスト用のデータ作成関数をtestUtilに切り出し

--- backend/src/app/sample/__tests__/pairs/pair.test.ts
+++ backend/src/app/sample/__tests__/pairs/pair.test.ts
@@ -1,30 +1,7 @@
-import * as faker from 'faker'
 import { Pair } from 'src/domain/pairs/pair'
-import { Participant } from 'src/domain/participants/participant'
-import { ParticipantMailAddress } from 'src/domain/participants/participantMailAddress'
-import { ParticipantName } from 'src/domain/participants/participantName'
 import { ParticipantStatus } from 'src/domain/participants/participantStatus'
 import { ParticipantID } from 'src/domain/participants/paticipantID'
-
-const mockedParticipant = (
-  props: {
-    id?: ParticipantID
-    name?: ParticipantName
-    mailAddress?: ParticipantMailAddress
-    status?: ParticipantStatus
-  } = {},
-) => {
-  return new Participant({
-    id: props.id ?? new ParticipantID(faker.random.number()),
-    name: props.name ?? new ParticipantName('hoge'),
-    mailAddress:
-      props.mailAddress ??
-      new ParticipantMailAddress(
-        `example${faker.random.number(100)}@example.com`,
-      ),
-    status: props.status ?? new ParticipantStatus('在籍中'),
-  })
-}
+import { mockedParticipant } from '../../../../../testUtil/domain/partcipant-factory' // なぜか相対パスじゃないと読み込まれない
 
 describe('ペアを作成できる', () => {
   it('1名以下の場合、無効であること', () => {
