4e60ea6610b0dd32c9ee53bb89141f1a54f80a77
YutoKashiwagi <kashiwagi19950714@gmail.com>
2022-09-07 20:23:33 +0900

    ペア名オブジェクトを作成

---
+++ backend/src/app/sample/__tests__/pairs/pairName.test.ts
@@ -0,0 +1,26 @@
+import { PairName } from "src/domain/pairs/pairName"
+
+describe('ペア名を作成できる', () => {
+  it('小文字のアルファベット一文字のペア名を作成できること', () => {
+    expect(new PairName('a').value).toEqual('a')
+    expect(new PairName('z').value).toEqual('z')
+  })
+
+  it('小文字のアルファベット以外は無効であること', () => {
+    expect(() => new PairName('A')).toThrow('ペア名は小文字のアルファベットのみ有効です')
+    expect(() => new PairName('!')).toThrow('ペア名は小文字のアルファベットのみ有効です')
+  })
+})
+
+describe('ペア名から次のペア名を作成できる', () => {
+  it('ペア名がaの場合、bを取得できること', () => {
+    const pairName = new PairName('a')
+    const nextPairName = new PairName('b')
+    expect(pairName.next()).toEqual(nextPairName)
+  })
+
+  it('ペア名がzの場合、エラーになること', () => {
+    const pairName = new PairName('z')
+    expect(() => pairName.next()).toThrow('これ以上ペアを作成できません')
+  })
+})
